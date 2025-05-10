d21da81496a32972fe43153260c18abcb773ae03
Mateusz Wojtasz <mateuszwojtasz1@gmail.com>
2025-05-10 23:49:29 +0200

    Refactor structure, add mvc catalogs

--- src/main/java/battleships/Battleships.java
+++ src/main/java/battleships/Battleships.java
@@ -1,4 +1,4 @@
-package com.example.test;
+package battleships;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Battleships extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Battleships.class.getResource("/battleships/hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
