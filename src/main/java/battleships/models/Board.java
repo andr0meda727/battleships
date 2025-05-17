db3672a4ee01e7d0f99b9ad83a319a9223053de3
Patryk Persak <abogusiac@gmail.com>
2025-05-17 17:56:17 +0200

    Add resetBoard method

--- src/main/java/battleships/models/Board.java
+++ src/main/java/battleships/models/Board.java
@@ -86,5 +86,12 @@ public class Board {
         }
     }
 
-
+    public void resetBoard() {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                board[i][j] = new Cell();
+            }
+        }
+        placeShips();
+    }
 }
