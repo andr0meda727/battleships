e8c5916c645e474349291141b68efc3b222d08ea
Patryk Persak <abogusiac@gmail.com>
2025-05-17 17:33:42 +0200

    Refactor board to use Cell and Ship objects instead of int[][]

--- src/main/java/battleships/models/Board.java
+++ src/main/java/battleships/models/Board.java
@@ -11,28 +11,30 @@ public class Board {
     }
 
     private static final int BOARD_SIZE = 10;
-    private int[][] board;
+    private Cell[][] board;
     final List<Integer> shipLengths = new ArrayList<>(Arrays.asList(2, 3, 3, 4, 5));
 
     public Board() {
-        board = new int[BOARD_SIZE][BOARD_SIZE];
+        board = new Cell[BOARD_SIZE][BOARD_SIZE];
         for (int i = 0; i < BOARD_SIZE; i++) {
             for (int j = 0; j < BOARD_SIZE; j++) {
-                board[i][j] = 0;
+                board[i][j] = new Cell();
             }
         }
+        placeShips();
     }
 
     private void placeShips() {
         for (int length : shipLengths) {
             boolean placed = false;
             while(!placed) {
-                int row = (int) (Math.random() * 10);
-                int col = (int) (Math.random() * 10);
+                int row = (int) (Math.random() * BOARD_SIZE);
+                int col = (int) (Math.random() * BOARD_SIZE);
                 Direction direction = ((int) ((Math.random() * 10) % 2)) == 0 ? Direction.VERTICAL : Direction.HORIZONTAL;
 
                 if(canPlaceShip(row, col, length, direction)) {
-                    changeIndexes(row, col, length, direction);
+                    Ship ship = new Ship(length);
+                    placeShipOnBoard(ship, row, col, length, direction);
                     placed = true;
                 }
             }
@@ -47,7 +49,10 @@ public class Board {
                 if (i < 0 || i >= BOARD_SIZE) continue;
                 for (int j = col - 1; j <= col + length; j++) {
                     if (j < 0 || j >= BOARD_SIZE) continue;
-                    if (board[i][j] == 1) return false;
+                    if (board[i][j].hasShip()) {
+                        System.out.println("The ship is already here");
+                        return false;
+                    }
                 }
             }
         }
@@ -58,23 +63,28 @@ public class Board {
                 if (i < 0 || i >= BOARD_SIZE) continue;
                 for (int j = col - 1; j <= col + 1; j++) {
                     if (j < 0 || j >= BOARD_SIZE) continue;
-                    if (board[i][j] == 1) return false;
+                    if (board[i][j].hasShip()) {
+                        System.out.println("The ship is already here");
+                        return false;
+                    }
                 }
             }
         }
         return true;
     }
 
-    private void changeIndexes(int row, int col, int length, Direction direction) {
+    private void placeShipOnBoard(Ship ship, int row, int col, int length, Direction direction) {
         if (direction == Direction.HORIZONTAL) {
             for (int i = 0; i < length; i++) {
-                board[row][col + i] = 1; // needed change to Object Ship
+                board[row][col + i].setShip(ship);
             }
         }
         else {
             for (int i = 0; i < length; i++) {
-                board[row + i][col] = 1; // needed change to Object Ship
+                board[row + i][col].setShip(ship);
             }
         }
     }
+
+
 }
